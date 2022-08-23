05a7be83ab5eb87a2405d462975d38a16115dc37
papsonic <le-grand-mannitout@protonmail.com>
2022-08-24 00:30:02 +0200

    Format code using ocamlformat.

--- src/better_chesscom.ml
+++ src/better_chesscom.ml
@@ -7,25 +7,21 @@ open Cohttp_lwt
 open Cohttp_lwt_jsoo
 open Js_of_ocaml
 
-type user = {
-  username: string;
-  elo: string
-}
+type user = { username : string; elo : string }
 
 type game = {
-  white: user;
-  black: user;
-  pgn: string;
-  opening: string;
-  time_control: string;
-  time_class: string;
-  is_black: bool;
-  has_won: bool
+  white : user;
+  black : user;
+  pgn : string;
+  opening : string;
+  time_control : string;
+  time_class : string;
+  is_black : bool;
+  has_won : bool;
 }
 
 let doc = Dom_html.window##.document
-
-let append_first n c = Dom.insertBefore n c (n##.firstChild)
+let append_first n c = Dom.insertBefore n c n##.firstChild
 let ( <++> ) = append_first
 let ( <+> ) = Dom.appendChild
 let ( <-> ) = Dom.removeChild
@@ -35,44 +31,52 @@ let json_of_body body =
   str |> Safe.from_string |> Lwt.return
 
 let get_json link =
-  link
-  |> Uri.of_string
-  |> Client.get
-  >>= fun (_, body) ->
-  json_of_body body
+  link |> Uri.of_string |> Client.get >>= fun (_, body) -> json_of_body body
 
 let post_urlencoded url key content =
   Client.post
-  ~body: (Body.of_string (key ^ "=" ^ content))
-  ~headers:
-  ((Cohttp.Header.init()) |>
-  (fun h -> Cohttp.Header.add h "Content-Type" "application/x-www-form-urlencoded; charset=UTF-8"))
-  (Uri.of_string url)
-  >>= fun (_, body) ->
-    json_of_body body
+    ~body:(Body.of_string (key ^ "=" ^ content))
+    ~headers:
+      ( Cohttp.Header.init () |> fun h ->
+        Cohttp.Header.add h "Content-Type"
+          "application/x-www-form-urlencoded; charset=UTF-8" )
+    (Uri.of_string url)
+  >>= fun (_, body) -> json_of_body body
 
 let get_monthly_games username =
   let get_last_from_json_list json key =
-    json
-    |> Safe.Util.member key |> Safe.Util.to_list
-    |> (fun lst -> List.nth (List.rev lst) 0) in
-  let* archives = get_json ("https://api.chess.com/pub/player/" ^ username ^ "/games/archives") in
-  let* games = get_last_from_json_list archives "archives" |> Safe.Util.to_string
-  |> get_json in
+    json |> Safe.Util.member key |> Safe.Util.to_list |> fun lst ->
+    List.nth (List.rev lst) 0
+  in
+  let* archives =
+    get_json ("https://api.chess.com/pub/player/" ^ username ^ "/games/archives")
+  in
+  let* games =
+    get_last_from_json_list archives "archives"
+    |> Safe.Util.to_string |> get_json
+  in
   games |> Safe.Util.member "games" |> Safe.Util.to_list |> Lwt.return
 
 let get_pgn json = json |> Safe.Util.member "pgn" |> Safe.Util.to_string
 
 let get_user json color =
   let profile = Safe.Util.member color json in
-  {username = profile |> Safe.Util.member "username" |> Safe.Util.to_string;
-   elo = profile |> Safe.Util.member "rating" |> Safe.Util.to_int |> string_of_int}
+  {
+    username = profile |> Safe.Util.member "username" |> Safe.Util.to_string;
+    elo =
+      profile |> Safe.Util.member "rating" |> Safe.Util.to_int |> string_of_int;
+  }
 
 let has_black_won json =
-  (json |> Safe.Util.member "black" |> Safe.Util.member "result" |> Safe.Util.to_string) = "win"
+  json |> Safe.Util.member "black" |> Safe.Util.member "result"
+  |> Safe.Util.to_string = "win"
 
 let user_black json username =
-  let black = json |> Safe.Util.member "black" |> Safe.Util.member "username" |> Safe.Util.to_string in
+  let black =
+    json |> Safe.Util.member "black"
+    |> Safe.Util.member "username"
+    |> Safe.Util.to_string
+  in
   black = username
 
 let get_time_class json =
@@ -80,24 +84,30 @@ let get_time_class json =
 
 let get_opening pgn =
   let re =
-    Re.seq [Re.str "openings/";
-            Re.group (Re.rep (Re.any));
-            Re.str {|[UTCD|}]
-    |> Re.compile |> (fun e -> Re.exec e pgn) in
-  re |> (fun e -> Re.Group.get e 1) 
-  |> Str.split (Str.regexp {|"|}) |> (fun lst -> List.nth lst 0)
+    Re.seq [ Re.str "openings/"; Re.group (Re.rep Re.any); Re.str {|[UTCD|} ]
+    |> Re.compile
+    |> fun e -> Re.exec e pgn
+  in
+  re
+  |> (fun e -> Re.Group.get e 1)
+  |> Str.split (Str.regexp {|"|})
+  |> (fun lst -> List.nth lst 0)
   |> Str.global_replace (Str.regexp "-") " "
 
 let get_time_control json =
   let time_and_increment =
-    json |> Safe.Util.member "time_control" |> Safe.Util.to_string
-    |> (fun str -> Str.split (Str.regexp "+") str) in
+    json |> Safe.Util.member "time_control" |> Safe.Util.to_string |> fun str ->
+    Str.split (Str.regexp "+") str
+  in
   let time_in_min =
-    (List.nth time_and_increment 0) |> int_of_string |> (fun n -> n/60) |> string_of_int in
+    List.nth time_and_increment 0
+    |> int_of_string
+    |> (fun n -> n / 60)
+    |> string_of_int
+  in
   match time_and_increment with
-    | [_; increment] ->
-      time_in_min ^ "+" ^ increment
-    | _ -> time_in_min ^ "+0"
+  | [ _; increment ] -> time_in_min ^ "+" ^ increment
+  | _ -> time_in_min ^ "+0"
 
 let format_pgn pgn =
   pgn
@@ -106,37 +116,39 @@ let format_pgn pgn =
   |> Str.global_replace (Str.regexp "\\n") ""
 
 let get_lichess_url pgn is_black =
-  let* json = post_urlencoded "https://lichess.org/api/import" "pgn" (format_pgn pgn) in
+  let* json =
+    post_urlencoded "https://lichess.org/api/import" "pgn" (format_pgn pgn)
+  in
   let str = json |> Safe.Util.member "url" |> Safe.Util.to_string in
-  if is_black then Lwt.return @@ str ^ "/black"
-  else Lwt.return str
+  if is_black then Lwt.return @@ str ^ "/black" else Lwt.return str
 
 let get_infos username game =
   let is_black = user_black game username in
   let black_won = has_black_won game in
   let pgn = get_pgn game in
   {
-  white = get_user game "white";
-  black = get_user game "black";
-  pgn = pgn;
-  opening = get_opening pgn;
-  time_control = get_time_control game;
-  time_class = get_time_class game;
-  is_black = is_black;
-  has_won = (black_won && is_black) || (not black_won && not is_black)
+    white = get_user game "white";
+    black = get_user game "black";
+    pgn;
+    opening = get_opening pgn;
+    time_control = get_time_control game;
+    time_class = get_time_class game;
+    is_black;
+    has_won = (black_won && is_black) || ((not black_won) && not is_black);
   }
 
 let main username =
   let* games = get_monthly_games username in
   let rec get_games = function
-    | hd :: tl ->
-      (get_infos username hd):: (get_games tl)
-    | _ -> [] in
+    | hd :: tl -> get_infos username hd :: get_games tl
+    | _ -> []
+  in
   get_games games |> Lwt.return
 
 let get_last_game username =
   let* games = get_monthly_games username in
-  games |> List.rev |> (fun lst -> List.nth lst 0)
+  games |> List.rev
+  |> (fun lst -> List.nth lst 0)
   |> get_infos username |> Lwt.return
 
 let get_search callback =
@@ -152,32 +164,32 @@ let get_search callback =
         else Js._true)
 
 let overview_string white black time_control time_class opening =
-  let user_format username elo =
-    username ^ " (" ^ elo ^ ")" in
-  time_control ^ " (" ^ time_class ^ ") : " ^
-  (user_format white.username white.elo) ^ " - " ^ (user_format black.username black.elo) ^
-  " : " ^ opening
+  let user_format username elo = username ^ " (" ^ elo ^ ")" in
+  time_control ^ " (" ^ time_class ^ ") : "
+  ^ user_format white.username white.elo
+  ^ " - "
+  ^ user_format black.username black.elo
+  ^ " : " ^ opening
 
 let games_screen page games username =
   let refresh = Dom_html.createButton doc in
-    refresh##.innerHTML := Js.string "Refresh";
+  refresh##.innerHTML := Js.string "Refresh";
   page <+> refresh;
   let games_div = Dom_html.createDiv doc in
   page <+> games_div;
   let display_game game =
     let div = Dom_html.createDiv doc in
-    if game.has_won
-      then div##.className := Js.string "overview_won"
-      else div##.className := Js.string "overview_lost";
+    if game.has_won then div##.className := Js.string "overview_won"
+    else div##.className := Js.string "overview_lost";
     let button = Dom_html.createButton doc in
     button##.innerHTML := Js.string "Analyse";
     let overview = Dom_html.createP doc in
     overview##.innerHTML :=
       Js.string
-        ("<a>" ^ (overview_string
-                  game.white
-                  game.black
-                  game.time_control game.time_class game.opening) ^ "</a>");
+        ("<a>"
+        ^ overview_string game.white game.black game.time_control
+            game.time_class game.opening
+        ^ "</a>");
     div <+> overview;
     div <+> button;
     games_div <++> div;
@@ -193,31 +205,33 @@ let games_screen page games username =
             Lwt.return_unit))
     in
     games_div <++> div;
-    Lwt.return_unit in
+    Lwt.return_unit
+  in
 
-  let* () = Lwt_list.iter_p
-    (fun game -> display_game game) games
-    |> Lwt.ignore_result |> Lwt.return in
+  let* () =
+    Lwt_list.iter_p (fun game -> display_game game) games
+    |> Lwt.ignore_result |> Lwt.return
+  in
 
-    let rec event () : 'a Lwt.t  =
-      Js_of_ocaml_lwt.Lwt_js_events.(
-        seq_loop click refresh (fun _ _ ->
+  let rec event () : 'a Lwt.t =
+    Js_of_ocaml_lwt.Lwt_js_events.(
+      seq_loop click refresh (fun _ _ ->
           let* game = get_last_game username in
           display_game game |> Lwt.ignore_result;
-          event ())) in
-    event () |> Lwt.ignore_result;
-    Lwt.return_unit
+          event ()))
+  in
+  event () |> Lwt.ignore_result;
+  Lwt.return_unit
 
 let onload _ =
   get_search (fun text ->
-  let page = Dom_html.getElementById "main" in
-  let login = Dom_html.getElementById "login-screen" in
-  page <-> login;
-  (let username = Js.to_string text in
-  let* gm = main username in
-  games_screen page gm username |> Lwt.ignore_result
-  |> Lwt.return)
-  |> Lwt.ignore_result);
+      let page = Dom_html.getElementById "main" in
+      let login = Dom_html.getElementById "login-screen" in
+      page <-> login;
+      (let username = Js.to_string text in
+       let* gm = main username in
+       games_screen page gm username |> Lwt.ignore_result |> Lwt.return)
+      |> Lwt.ignore_result);
   Js._false
 
 let () = Dom_html.window##.onload := Dom_html.handler onload
